d6986fec1357f83e175519e89168904f695d0683
Teddaudi <daudited@gmail.com>
2024-04-01 17:41:05 +0300

    theme color update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,7 +17,7 @@ export default async function Hero() {
     return (
         <div>
             {Hero.map((Hero,id) => (
-                <div className="pt-24 gradient" key={id}>
+                <div className="pt-24 green text-white" key={id}>
                     <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                         <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
                             <p className="uppercase tracking-loose w-full" key={id}>{Hero.smallTitle}</p>
@@ -27,7 +27,7 @@ export default async function Hero() {
                             <p className="leading-normal text-2xl mb-8">
                                 {Hero.text}
                             </p>
-                            <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                            <button className="mx-auto lg:mx-0 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 hover:bg-green-400 hover:text-white duration-300 ease-in-out">
                                 {Hero.buttonText}
                             </button>
                         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = () => {
         setToggleNavbar((prev) => !prev)
     }
     return (
-        <nav id="header" className="fixed w-full z-10 top-0">
+        <nav id="header" className="fixed w-full z-10 top-0 text-white green">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                 <div className="pl-4 flex items-center">
-                    <Link className="toggleColour text no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+                    <Link className="toggleColour no-underline hover:no-underline font-semibold text-2xl " href="/">
                         <svg className="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
                             <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
                             <path
@@ -19,11 +19,11 @@ const Navbar = () => {
                                 d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
                             />
                         </svg>
-                        LANDING
+                        TODO HEALTHCARE
                     </Link>
                 </div>
                 <div className="block lg:hidden pr-4">
-                    <button onClick={handleToggleNavbarClick} id="nav-toggle" className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                    <button onClick={handleToggleNavbarClick} id="nav-toggle" className="flex items-center p-1 text-gray-400 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                         <svg className="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <title>Menu</title>
                             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -31,26 +31,26 @@ const Navbar = () => {
                     </button>
                 </div>
                 <div className={toggleNavbar ?
-                    "w-full flex-grow lg:flex lg:items-center lg:w-auto  mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" :
-                    "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"} id="nav-content">
+                    "w-full flex-grow lg:flex lg:items-center lg:w-auto  mt-2 lg:mt-0 green lg:bg-transparent text-white p-4 lg:p-0 z-20" :
+                    "w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 green lg:bg-transparent text-black p-4 lg:p-0 z-20"} id="nav-content">
                     <ul className="list-reset lg:flex justify-end flex-1 items-center">
                         <li className="mr-3">
-                            <Link className="inline-block py-2 px-4 text-black font-bold text-lg hover:text-red-800 no-underline" href="/">Home</Link>
+                            <Link className="inline-block py-2 px-4 text-white font-bold text-lg hover:text-red-800 no-underline" href="/">Home</Link>
                         </li>
                         <li className="mr-3">
-                            <Link className="inline-block text-black no-underline font-bold text-lg hover:text-red-800 hover:text-underline py-2 px-4" href="/what-we-do">What We Do</Link>
+                            <Link className="inline-block text-white no-underline font-bold text-lg hover:text-red-800 hover:text-underline py-2 px-4" href="/what-we-do">What We Do</Link>
                         </li>
                         <li className="mr-3">
-                            <Link className="inline-block text-black no-underline font-bold text-lg hover:text-red-800 hover:text-underline py-2 px-4" href="/about-us">About Us</Link>
+                            <Link className="inline-block text-white no-underline font-bold text-lg hover:text-red-800 hover:text-underline py-2 px-4" href="/about-us">About Us</Link>
                         </li>
                         <li className="mr-3">
-                            <Link className="inline-block text-black no-underline font-bold text-lg hover:text-red-800 hover:text-underline py-2 px-4" href="/contact-us">Contact Us</Link>
+                            <Link className="inline-block text-white no-underline font-bold text-lg hover:text-red-800 hover:text-underline py-2 px-4" href="/contact-us">Contact Us</Link>
                         </li>
                     </ul>
                     <Link href={'/appointments'}>
                         <button
                             id="navAction"
-                            className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+                            className="mx-auto lg:mx-0 hover:bg-green-400 hover:text-white bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
                         >
                             Book an Appointment
                         </button>
