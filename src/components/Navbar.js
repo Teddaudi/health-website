bc49632fa8be7a578fbbf5f892260670dde9181f
Teddaudi <daudited@gmail.com>
2024-04-11 07:47:41 +0300

    navbar padding removal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div className="block lg:hidden mr-4">
-                    <button onClick={handleToggleNavbarClick} id="nav-toggle" className="flex items-center p-1 text-gray-400 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                    <button onClick={handleToggleNavbarClick} id="nav-toggle" className="flex items-center text-gray-400 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                         <svg className="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <title>Menu</title>
                             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
