31c19e980d8ab3244821850da994f2bf0900bda8
Teddaudi <daudited@gmail.com>
2024-04-11 08:48:51 +0300

    Names

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,62 +17,61 @@ async function getData(){
 }
 export default async function Main() {
     const whyData = await getData();
-    console.log(whyData, 'why data')
     return (
         <section className="text-gray-600 body-font " style={{ backgroundColor: '#fffeeb' }}>
             { whyData.map((why,id)=>(
-                <div className="container px-5 py-10 mx-auto" key={id}>
+                <div className="container px-5 py-10 mx-auto" key={1}>
                 <div className="flex flex-wrap w-full mb-5 flex-col items-center text-center">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
                         Why Us?
                     </h1>
                 </div>
-                <div class="grid-cols-1 sm:grid md:grid-cols-3 mx-auto" key={id}>
+                <div className="grid-cols-1 sm:grid md:grid-cols-3 mx-auto" key={2}>
                     <div style={{ backgroundColor: '#d3d3d3' }}
-                        class="mx-6 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
+                        className="mx-6 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
                         <a href="#!">
                             <img
-                                key={id}
-                                class="rounded-t-lg"
+                                key={3}
+                                className="rounded-t-lg"
                                 src={urlFor(why.image1).url()}
                                 alt="Hollywood Sign on The Hill" />
                         </a>
-                        <div class="p-6">
-                            <h5 class="mb-2 text-xl font-medium leading-tight" key={id}>{why.title1}</h5>
-                            <p class="mb-4 text-base" key={id}>
+                        <div className="p-6">
+                            <h5 className="mb-2 text-xl font-medium leading-tight" key={5}>{why.title1}</h5>
+                            <p className="mb-4 text-base" key={4}>
                                {why.text1}
                             </p>
                         </div>
                     </div>
 
                     <div style={{ backgroundColor: '#d3d3d3' }}
-                        class="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
+                        className="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
                         <a href="#!">
                             <img
-                                key={id}
-                                class="rounded-t-lg"
+                                key={6}
+                                className="rounded-t-lg"
                                 src={urlFor(why.image2).url()}
                                 alt="Palm Springs Road" />
                         </a>
-                        <div class="p-6">
-                            <h5 class="mb-2 text-xl font-medium leading-tight" key={id}>{why.title2}</h5>
-                            <p class="mb-4 text-base" key={id}>
+                        <div className="p-6">
+                            <h5 className="mb-2 text-xl font-medium leading-tight" key={8}>{why.title2}</h5>
+                            <p className="mb-4 text-base" key={9}>
                                 {why.text2}
                             </p>
                         </div>
                     </div>
                     <div style={{ backgroundColor: '#d3d3d3' }}
-                        class="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
+                        className="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0">
                         <a href="#!">
                             <img
-                                key={id}
-                                class="rounded-t-lg"
+                                key={10}
+                                className="rounded-t-lg"
                                 src={urlFor(why.image3).url()}
                                 alt="Los Angeles Skyscrapers" />
                         </a>
-                        <div class="p-6" >
-                            <h5 class="mb-2 text-xl font-medium leading-tight" key={id}>{why.title3}</h5>
-                            <p class="mb-4 text-base" key={id}>
+                        <div className="p-6" >
+                            <h5 className="mb-2 text-xl font-medium leading-tight" key={11}>{why.title3}</h5>
+                            <p className="mb-4 text-base" key={12}>
                                 {why.text3}
                             </p>
                         </div>
