f7a9bde56c549855df85a04abbaace7df25561e8
Teddaudi <daudited@gmail.com>
2024-05-05 10:26:06 +0300

    readmore

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { client, urlFor } from '../lib/client';
 
 async function getData() {
@@ -16,7 +17,7 @@ export default async function Hero() {
     const Hero = await getData();
     return (
         <div>
-            {Hero.map((Hero,id) => (
+            {Hero.map((Hero, id) => (
                 <div className="pt-24 green text-white" key={id}>
                     <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                         <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
@@ -27,9 +28,11 @@ export default async function Hero() {
                             <p className="leading-normal text-2xl mb-8">
                                 {Hero.text}
                             </p>
-                            <button className="mx-auto lg:mx-0 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 hover:bg-green-400 hover:text-white duration-300 ease-in-out">
-                                {Hero.buttonText}
-                            </button>
+                            <Link href={'/about-us'}>
+                                <button className="mx-auto lg:mx-0 bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 hover:bg-green-400 hover:text-white duration-300 ease-in-out">
+                                    {Hero.buttonText}
+                                </button>
+                            </Link>
                         </div>
                         <div className="w-full md:w-3/5 py-6 text-center">
                             <img className="w-full md:w-4/5 z-50" src={urlFor(Hero.image).url()} alt="" />
